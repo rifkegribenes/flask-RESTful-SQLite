26f45aecb26e8853d236a119f612823684ca0c22
rifkegribenes <rifkegribenes@gmail.com>
2019-02-03 13:22:03 -0800

    put method

--- code/item.py
+++ code/item.py
@@ -73,13 +73,31 @@ class Item(Resource):
 	def put(self, name):
 		data = Item.parser.parse_args()
 
-		item = next(filter(lambda x: x['name'] == name, items), None)
+		item = self.find_by_name(name)
+		updated_item = {'name': name, 'price': data['price']}
+
 		if item is None:
-			item = {'name': name, 'price': data['price']}
-			items.append(item)
+			try:
+				self.insert(item)
+			except:
+				return {"message": "An error occurred inserting the item."}, 500
 		else:
-			item.update(data)
-		return item
+			try:
+				self.update(updated_item)
+			except:
+				return {"message": "An error occurred updating the item."}, 500
+		return updated_item
+
+	@classmethod
+	def update(cls, item):
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+
+		query = "UPDATE items SET price=? WHERE name=?"
+		cursor.execute(query, (item['price'], item['name'],))
+
+		connection.commit()
+		connection.close()
 
 class ItemList(Resource):
 	def get(self):
